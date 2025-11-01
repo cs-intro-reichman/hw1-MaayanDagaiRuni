87d0460d8f1b9cc0492cf8a6f2fd03e717a11bf9
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-01 21:22:38 +0200

    Fixed print statment in FVCalc

--- FVCalc.java
+++ FVCalc.java
@@ -2,16 +2,18 @@
 
 public class FVCalc {
 	public static void main(String[] args){
-		// Gets three 
+		// Gets current value, rate in percentage and years
 		int currentValue = Integer.parseInt(args[0]);
-		int rate = Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 
-		double intrest_rate = ((double) rate)/100 ;
+		// Calcs the rate precentage 
+		double intrest_rate = rate/100 ;
 
+		// Calcs the Furutre Value
 		double FutureValue = currentValue*(Math.pow((1+ intrest_rate),n));
 
-		System.out.println("After " + n + " years, " + currentValue + " saved at " + rate +
-						   "% will yield " + (int)FutureValue + "$.");		
+		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate +
+						   "% will yield " +"$" + (int)FutureValue + "");		
 	}
 }
