98158797d78e22b552ac2ae9ae5e14d8d185e0b0
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-01 21:15:14 +0200

    Fixed tens and houndreds in NumWords

--- NumWords.java
+++ NumWords.java
@@ -3,8 +3,8 @@ public class NumWords {
 	public static void main(String args[]) {
 		int value = Integer.parseInt(args[0]);
 		int ones = value % 10;
-		int tens = ((value % 100)- ones)/10;
-		int hundreds = ((value % 1000)- tens -ones)/100;
+		int tens = (value / 10 ) % 10;
+		int hundreds = value /100;
 
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and "+ ones + " ones.");
 	}
